9a0368b1308d879b5cbf348ee8b3dde81ee758fa
M Durrant <mdurrant@DN52em8p.SUNet>
2017-05-17 12:14:12 -0600

    update

--- classify/scripts/install_packages.R
+++ classify/scripts/install_packages.R
@@ -1,6 +1,6 @@
 if (!("mlr" %in% installed.packages()[,"Package"])){
-  install.packages("mlr")  
-  install.packages("randomForest")  
+  install.packages("mlr", repos=c("https://cran.cnr.berkeley.edu/"))  
+  install.packages("randomForest", repos=c("https://cran.cnr.berkeley.edu/"))  
 }
 
 library(tidyr)
@@ -8,3 +8,4 @@ args = commandArgs(trailingOnly=TRUE)
 
 output_file <- args[1]
 write_tsv(data.frame(Done=c()), output_file)
+?install.packages
