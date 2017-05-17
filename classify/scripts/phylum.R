a3a011349154453ea3d583496d559d0fbda7de9c
M Durrant <mdurrant@DN52em8p.SUNet>
2017-05-17 12:07:42 -0600

    update

--- classify/scripts/phylum.R
+++ classify/scripts/phylum.R
@@ -1,9 +1,38 @@
 if (!("mlr" %in% installed.packages()[,"Package"])){
   install.packages("mlr")  
+  install.packages("randomForest")  
 }
+library(mlr)
+library(readr)
+library(dplyr)
+
 args = commandArgs(trailingOnly=TRUE)
 
-seqtab <- readRDS(args[1], refhook = NULL)
-silva_train_set <- args[2]
-silva_species_train_set <- args[3]
-silva_taxa_out_rds <- args[4]
+features_dir <- args[1]
+output_dir <- args[2]
+
+dir.create(output_dir, recursive=TRUE)
+
+features <- list.files(features_dir, full.names=T)
+features <- features[grepl("R1", features)]
+
+max_lines <- 100
+n_max <-as.integer(max_lines / length(features))
+training_set <- NULL
+for (f in features){
+  data_in <- read_tsv(f, n_max=n_max) %>% select(-Seq, -Kingdom, -Class, -Order, -Family, -Genus, -Species)
+
+  if (is.null(train_data)){
+    training_set <- data_in
+  }
+  
+  training_set <- rbind(training_set, train_data)
+}
+
+classif.lrn = makeLearner("classif.randomForest", predict.type = "prob", fix.factors.prediction = TRUE)
+classif.task = makeClassifTask(data = data.frame(train_dataset), target = "Phylum")
+
+mod = train(classif.lrn, classif.task)
+
+out_rds <- file.path(output_dir, 'phylum.randomForest.rds')
+saveRDS(mod, out_rds)
