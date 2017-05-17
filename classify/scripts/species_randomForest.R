c38c0efe3fce5aea0b1493ea4ef437c94eba9818
M Durrant <mdurrant@DN52em8p.SUNet>
2017-05-17 12:50:32 -0600

    update

---
+++ classify/scripts/species_randomForest.R
@@ -0,0 +1,38 @@
+library(mlr)
+library(readr)
+library(dplyr)
+
+args = commandArgs(trailingOnly=TRUE)
+
+features_dir <- args[1]
+output_dir <- args[2]
+
+dir.create(output_dir, recursive=TRUE)
+
+features <- list.files(features_dir, full.names=T)
+features <- features[grepl("R1", features)]
+
+max_lines <- 1000000
+n_max <-as.integer(max_lines / length(features))
+training_set <- NULL
+
+counter <- 1
+for (f in features){
+  print(paste("Reading in file ", counter, "of", length(features)))
+  data_in <- read_tsv(f, n_max=n_max) %>% select(-Seq, -Kingdom, -Phylum, -Class, -Order, -Family, -Genus)
+  
+  if (is.null(data_in)){
+    training_set <- data_in
+  }
+  
+  training_set <- rbind(training_set, data_in)
+  counter <- counter + 1
+}
+
+classif.lrn = makeLearner("classif.randomForest", predict.type = "prob", fix.factors.prediction = TRUE)
+classif.task = makeClassifTask(data = data.frame(train_dataset), target = "Species")
+
+mod = train(classif.lrn, classif.task)
+
+out_rds <- file.path(output_dir, 'species.randomForest.rds')
+saveRDS(mod, out_rds)
